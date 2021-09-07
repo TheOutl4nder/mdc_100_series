6105b97f088f56af5b3c9c97f46d9406281db811
The Outl4nder <theoutl4nder@gmail.com>
2021-09-07 18:50:55 -0500

    mdc 3 done
    issues found on applying color to homepage, issue already reported here:
    https://github.com/material-components/material-components-flutter-codelabs/issues/258

--- lib/login.dart
+++ lib/login.dart
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'colors.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -24,6 +25,24 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _unfocusedColor = Colors.grey[600];
+  final _usernameFocusNode = FocusNode();
+  final _passwordFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _usernameFocusNode.addListener(() {
+        setState(() { 
+          //Redraw so that the username label reflects the focus state
+        });
+    });
+    _passwordFocusNode.addListener(() {
+        setState(() { 
+          //Redraw so that the password label reflects the focus state
+        });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,56 +54,74 @@ class _LoginPageState extends State<LoginPage> {
             const SizedBox(height: 80.0),
             Column(
               children: <Widget>[
-                Image.asset('assets/diamond.png'),
+                Image.asset('assets/diamond.png', color: kShrineBlack),
                 const SizedBox(height: 16.0),
-                const Text('SHRINE'),
+                Text(
+                  'SHRINE',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
               ],
             ),
             const SizedBox(height: 120.0),
-            // [Name]
             TextField(
-                controller: _usernameController,
-                decoration: const InputDecoration(
-                filled: true,
+              controller: _usernameController,
+              decoration: InputDecoration(
                 labelText: 'Username',
+                labelStyle: TextStyle(
+                    color: _usernameFocusNode.hasFocus
+                      ? Theme.of(context).colorScheme.secondary
+                      : _unfocusedColor),
               ),
+              focusNode: _usernameFocusNode,
             ),
-            // spacer
             const SizedBox(height: 12.0),
-            // [Password]
             TextField(
               controller: _passwordController,
-              decoration: const InputDecoration(
-                filled: true,
+              decoration: InputDecoration(
                 labelText: 'Password',
+                labelStyle: TextStyle(
+                    color: _passwordFocusNode.hasFocus
+                      ? Theme.of(context).colorScheme.secondary
+                      : _unfocusedColor),
               ),
-              obscureText: true,
+              focusNode: _passwordFocusNode,
             ),
-            // TODO: Wrap Username with AccentColorOverride (103)
-            // TODO: Remove filled: true values (103)
-            // TODO: Wrap Password with AccentColorOverride (103)
-            // TODO: Add TextField widgets (101)
             ButtonBar(
             // TODO: Add a beveled rectangular border to CANCEL (103)
             children: <Widget>[
               TextButton(
                 child: const Text('CANCEL'),
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(
+                    Theme.of(context).colorScheme.secondary,
+                  ),
+                  shape: MaterialStateProperty.all(
+                    const BeveledRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                    ),
+                  ),
+                ),
                 onPressed: () {
                       _usernameController.clear();
                       _passwordController.clear();
                 },
               ),
-              // TODO: Add an elevation to NEXT (103)
-              // TODO: Add a beveled rectangular border to NEXT (103)
               ElevatedButton(
                 child: const Text('NEXT'),
+                style: ButtonStyle(
+                  elevation: MaterialStateProperty.all(8.0),
+                  shape: MaterialStateProperty.all(
+                    const BeveledRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                    ),
+                  ),
+                ),
                 onPressed: () {
                   Navigator.pop(context);
                 },
               ),
             ],
-),
-
+            ),
           ],
         ),
       ),
